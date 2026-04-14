45aae57d0f4aba93dc1053f91884638cab409063
Rafael Ouverney <82585766+RafaelOuverney@users.noreply.github.com>
2026-04-14 15:19:52 -0300

    test: rejeitar senha sem letra maiúscula

--- __test__/service/verificarSenha.test.js
+++ __test__/service/verificarSenha.test.js
@@ -3,14 +3,20 @@ const {Senha} = require("../../src/model/senha.js");
 
 describe("validar senha", () => {
     test('rejeitar senha com menos de 8 caracteres', () => {
-        const senha = new Senha("12345");
+        const senha = new Senha("abcde");
         const resultado = ServicoSenha.verificarSenha(senha.senha);
         expect(resultado).toBe(false);  
     });
 
     test('aceitar senha com 8 ou mais caracteres', () => {
-        const senha = new Senha("12345678");
+        const senha = new Senha("Abcdefgh");
         const resultado = ServicoSenha.verificarSenha(senha.senha);
         expect(resultado).toBe(true);  
     });
+
+    test('rejeitar senha sem letra Maiúscula', ()=> {
+        const senha = new Senha("abcdefgh");
+        const resultado = ServicoSenha.verificarSenha(senha.senha);
+        expect(resultado).toBe(false);
+    })
 });
