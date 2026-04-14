552729ea1039b118e59d6fe275de7933325e6746
Rafael Ouverney <82585766+RafaelOuverney@users.noreply.github.com>
2026-04-14 14:54:10 -0300

    fix: correção do teste

--- __test__/service/verificarSenha.test.js
+++ __test__/service/verificarSenha.test.js
@@ -1,7 +1,10 @@
+const {ServicoSenha} = require("../../src/service/servicoSenha.js");
+const {Senha} = require("../../src/model/senha.js");
+
 describe("validar senha", () => {
     test('rejeitar senha com menos de 8 caracteres', () => {
-        const senha = new Senha("testesenha");
-        const resultado = senha.verificarSenha();
+        const senha = new Senha("123456");
+        const resultado = ServicoSenha.verificarSenha(senha);
         expect(resultado).toBe(false);
     });
 });
