7bcfb284637172d6eedf8e28f199f52295e6bd8f
Rafael Ouverney <82585766+RafaelOuverney@users.noreply.github.com>
2026-04-14 15:35:55 -0300

    test: outros testes para combinações de senha com e sem letras maiúsculas e minúsculas

--- __test__/service/verificarSenha.test.js
+++ __test__/service/verificarSenha.test.js
@@ -32,4 +32,46 @@ describe("validar senha", () => {
         expect(resultado).toBe(false);
     });
 
+    test('aceitar senha com letra minúscula', ()=> {
+        const senha = new Senha("Abcdefgh");
+        const resultado = ServicoSenha.verificarSenha(senha.senha);
+        expect(resultado).toBe(true);
+    });
+
+    test('rejeitar senha sem letra maiúscula e minúscula', ()=> {
+        const senha = new Senha("12345678");
+        const resultado = ServicoSenha.verificarSenha(senha.senha);
+        expect(resultado).toBe(false);
+    });
+
+    test('aceitar senha com letra maiúscula e minúscula', ()=> {
+        const senha = new Senha("Abcdefgh");
+        const resultado = ServicoSenha.verificarSenha(senha.senha);
+        expect(resultado).toBe(true);
+    });
+
+    test('rejeitar senha com menos de 8 caracteres e sem letra maiúscula e minúscula', ()=> {
+        const senha = new Senha("abc");
+        const resultado = ServicoSenha.verificarSenha(senha.senha);
+        expect(resultado).toBe(false);
+    });
+
+    test('rejeitar senha com menos de 8 caracteres e sem letra maiúscula', ()=> {
+        const senha = new Senha("abcdefg");
+        const resultado = ServicoSenha.verificarSenha(senha.senha);
+        expect(resultado).toBe(false);
+    });
+
+    test('rejeitar senha com menos de 8 caracteres e sem letra minúscula', ()=> {
+        const senha = new Senha("ABCDEFG");
+        const resultado = ServicoSenha.verificarSenha(senha.senha);
+        expect(resultado).toBe(false);
+    });
+
+    test('rejeitar senha com 8 ou mais caracteres e sem letra maiúscula', ()=> {
+        const senha = new Senha("abcdefgh");
+        const resultado = ServicoSenha.verificarSenha(senha.senha);
+        expect(resultado).toBe(false);
+    });
+
 });
